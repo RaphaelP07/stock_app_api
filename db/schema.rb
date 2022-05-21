d87a3bb6a3212387fb1c8074fd1f3be534bbc42f
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-05-21 15:05:01 +0800

    added jti

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_14_040635) do
     t.string "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["jti"], name: "index_users_on_jti", unique: true
+    t.index ["jti"], name: "index_us`ers_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
