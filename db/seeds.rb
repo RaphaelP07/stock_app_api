8def936a4aef6ede9c974296008dbd1600ac6787
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-05-26 14:45:31 +0800

    config iex endpoints in dotenv

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
 BASE_URL = "https://sandbox.iexapis.com"
-list_of_stocks = "#{BASE_URL}/beta/ref-data/symbols?token=#{ENV["IEX_TEST_KEY"]}"
-debugger
+list_of_symbols = "#{BASE_URL}/beta/ref-data/symbols?token=#{ENV["IEX_TEST_KEY"]}"
+top_ten_active = "#{BASE_URL}/stable/stock/market/list/mostactive?token=#{ENV["IEX_TEST_KEY"]}"
+top_three_gainers = "#{BASE_URL}/stable/stock/market/list/gainers?token=#{ENV["IEX_TEST_KEY"]}"
+stock_info = "#{BASE_URL}/stable/stock/fb/quote?token=#{ENV["IEX_TEST_KEY"]}"
+
+# top_ten_array = JSON.parse(RestClient.get(top_ten_active))
+# top_ten_array.each do |item| p "#{item["symbol"]} => $#{item["latestPrice"]}" end
+# debugger
