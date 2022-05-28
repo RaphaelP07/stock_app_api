41168de3726e58f3760c236fe51a6a37e9b3cbf8
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-05-28 23:41:26 +0800

    added route for buying

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/stocks/top_ten' => 'stocks#top_ten', as: 'top_ten'
   get '/stocks/info/:symbol' => 'stocks#info', as: 'info'
+  post '/wallets/:wallet_id/transactions/buy/:symbol/:shares' => 'transactions#buy', as: 'buy'
   resources :stocks
   get '/current_user', to: 'current_user#index'
   devise_for :users, controllers: { confirmations: 'confirmations' }, path: '', path_names: {
