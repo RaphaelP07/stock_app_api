0a2b90141af172c6a333ca75653651582e3ac3a5
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-05-30 20:54:07 +0800

    filtered api call results

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,10 +1,24 @@
 class Stock < ApplicationRecord
   def self.top_ten
-    top_ten_array = JSON.parse(RestClient.get("#{ENV["IEX_BASE_URL"]}/stable/stock/market/list/mostactive?token=#{ENV["IEX_TEST_KEY"]}"))
+    top_ten = (JSON.parse(RestClient.get("#{ENV["IEX_BASE_URL"]}/stable/stock/market/list/mostactive?token=#{ENV["IEX_TEST_KEY"]}"))).map { |x| 
+      x.select {
+        |key|
+        key == 'companyName' ||
+        key == 'iexOpen' || 
+        key == 'latestPrice' ||
+        key == 'symbol' 
+      }
+    }
   end
 
   def self.info(symbol)
-    stock_info = JSON.parse(RestClient.get("#{ENV["IEX_BASE_URL"]}/stable/stock/#{symbol}/quote?token=#{ENV["IEX_TEST_KEY"]}"))
+    stock_info = (JSON.parse(RestClient.get("#{ENV["IEX_BASE_URL"]}/stable/stock/#{symbol}/quote?token=#{ENV["IEX_TEST_KEY"]}"))).select { 
+      |key| 
+      key == 'companyName' ||
+      key == 'iexOpen' || 
+      key == 'latestPrice' ||
+      key == 'symbol' 
+    }
   end
   belongs_to :user
 end
