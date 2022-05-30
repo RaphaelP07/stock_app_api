82551acb93625e18b67b85f9a92e931cfcd24354
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-05-30 20:48:44 +0800

    fixed buy and sell methods

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,5 @@
 class Transaction < ApplicationRecord
-  validates :symbol, presence: true
+  # validates :symbol, presence: true
   validates :shares, comparison: { greater_than: 0 }
 
   belongs_to(
@@ -10,35 +10,42 @@ class Transaction < ApplicationRecord
 
   def self.buy(wallet_id, symbol, shares)
     stock_info = Stock.info(symbol)
-    if @transaction.wallet['balance'] >= (shares*stock_info['latestPrice']) && @transaction.save
-      @transaction = Wallet.find(wallet_id).transactions.build(
+    wallet = Wallet.find(wallet_id)
+    amount = shares*stock_info['latestPrice']
+    if wallet.update!(balance: wallet['balance'] - amount)
+      @transaction = wallet.transactions.build(
         symbol: stock_info['symbol'],
         company: stock_info['companyName'],
         price: stock_info['latestPrice'],
         shares: shares,
-        amount: shares*stock_info['latestPrice'],
+        amount: amount,
         action: 'buy',
         wallet_id: wallet_id
         )
-      @transaction.wallet.update(balance: @transaction.wallet['balance'] - @transaction[amount])
       @transaction.save
       @transaction
     else
-      @transaction.errors
+      wallet.errors
     end
   end
 
   def self.sell(wallet_id, symbol, shares)
     stock_info = Stock.info(symbol)
-    @transaction = Wallet.find(wallet_id).transactions.build(
-      symbol: stock_info['symbol'], 
-      company: stock_info['companyName'], 
-      price: stock_info['latestPrice'], 
-      shares: shares, 
-      amount: shares*stock_info['latestPrice'], 
-      action: 'sell',
-      wallet_id: wallet_id
-      )
-    @transaction.save
-  end
+    wallet = Wallet.find(wallet_id)
+    amount = shares*stock_info['latestPrice']
+    if wallet.update!(balance: wallet['balance'] + amount)
+      @transaction = wallet.transactions.build(
+        symbol: stock_info['symbol'],
+        company: stock_info['companyName'],
+        price: stock_info['latestPrice'],
+        shares: shares,
+        amount: amount,
+        action: 'sell',
+        wallet_id: wallet_id
+        )
+      @transaction.save
+      @transaction
+    else
+      wallet.errors
+    end
 end
