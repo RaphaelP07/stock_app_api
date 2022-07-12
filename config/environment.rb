5545476d61f09b1d099d3d26b982eee72b5f30ee
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-07-12 18:32:57 +0800

    fixing deployment

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@ ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'localhost:3000',
+  domain:               'https://raphdev-stock-app-api.herokuapp.com/',
   user_name:            ENV['MAILER_EMAIL'],
   password:             ENV['MAILER_PASS'],
   authentication:       'plain',
