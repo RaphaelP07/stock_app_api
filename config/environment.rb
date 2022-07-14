2ab9d0fcdf4cb05ce346c6fcb131abbfd9109736
RaphaelP07 <raphaelpabicopadua@gmail.com>
2022-07-14 09:35:08 +0800

    fixing mailer in deployment

--- config/environment.rb
+++ config/environment.rb
@@ -4,15 +4,15 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.perform_deliveries = true
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'https://raphdev-stock-app-api.herokuapp.com/',
-  user_name:            ENV['MAILER_EMAIL'],
-  password:             ENV['MAILER_PASS'],
-  authentication:       'plain',
-  enable_starttls_auto: true,
-  open_timeout:         5,
-  read_timeout:         5 }
+# ActionMailer::Base.perform_deliveries = true
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = {
+#   address:              'smtp.gmail.com',
+#   port:                 587,
+#   domain:               'https://raphdev-stock-app-api.herokuapp.com/',
+#   user_name:            ENV['MAILER_EMAIL'],
+#   password:             ENV['MAILER_PASS'],
+#   authentication:       'plain',
+#   enable_starttls_auto: true,
+#   open_timeout:         5,
+#   read_timeout:         5 }
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,4 +96,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://raphdev-stock-app-api.herokuapp.com/'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'heroku.com',
+    user_name:            ENV['MAILER_EMAIL'],
+    password:             ENV['MAILER_PASS'],
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    open_timeout:         5,
+    read_timeout:         5 
+  }
 end
